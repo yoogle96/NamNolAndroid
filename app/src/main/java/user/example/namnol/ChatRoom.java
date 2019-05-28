f6bb6b680c7c42ddc3388a2f9c39c752339fc48d
yoogle96 <dmltjs851@gmail.com>
2019-05-29 03:08:53 +0900

    added 채팅방

--- app/src/main/java/user/example/namnol/ChatRoom.java
+++ app/src/main/java/user/example/namnol/ChatRoom.java
@@ -1,14 +1,124 @@
 package user.example.namnol;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 public class ChatRoom extends AppCompatActivity {
+
+    private ListView listView;
+    private Button btnCreate;
+
+    private ArrayAdapter<String> arrayAdapter;
+    private ArrayList<String> arrRoomList = new ArrayList<>();
+    private DatabaseReference reference = FirebaseDatabase.getInstance().getReference().getRoot();
+    private String name;
+    private String strName;
+    private String strRoom;
+
+    Map<String, Object> map = new HashMap<String, Object>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("랜덤채팅 APP");
         setContentView(R.layout.activity_chat_room);
+
+        // 로그인화면에서 닉네임을 가져온다.
+        Intent intent = getIntent();
+        strName = intent.getStringExtra("name");
+
+        listView = (ListView) findViewById(R.id.list);
+        btnCreate = findViewById(R.id.btn_create);
+
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrRoomList);
+        listView.setAdapter(arrayAdapter);
+
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final EditText et_inDialog = new EditText(ChatRoom.this);
+
+                final AlertDialog.Builder builder = new AlertDialog.Builder(ChatRoom.this);
+                builder.setTitle("채팅방 이름 입력");
+                builder.setView(et_inDialog);
+                builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        strRoom = et_inDialog.getText().toString();
+                        map.put(strRoom, "");
+                        reference.updateChildren(map);
+                    }
+                });
+                builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                builder.show();
+            }
+        });
+
+        // 특정 경로의 전체 내용에 대한 변경 사항을 읽고 수신 대기함
+        // onDateChange는 Database가 변경되었을 때 호출되고
+        // onCancelled는 취소됐을때 호출된다.
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Set<String> set = new HashSet<String>();
+                Iterator i = dataSnapshot.getChildren().iterator();
+
+                while (i.hasNext()) {
+                    set.add(((DataSnapshot) i.next()).getKey());
+                }
+
+                arrRoomList.clear();
+                arrRoomList.addAll(set);
+
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        // 리스트뷰의 채팅방을 클릭했을 때 반응
+        // 채팅방의 이름과 입장하는 유저의 이름을 전달
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(getApplicationContext(), Chat.class);
+                intent.putExtra("roomName", ((TextView) view).getText().toString());
+                intent.putExtra("userName", strName);
+                startActivity(intent);
+            }
+        });
     }
 }
+
